181e737c2775e0c1fb346d7cf4c0e07216e9af5b
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-25 13:10:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,26 @@ class Song
   def self.alphabetical
     @@all.sort_by {|element| element.name}
   end
-  def self.new_from_filename(file_name)
-    song = self.new 
-    song.
+   def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
     
 end
