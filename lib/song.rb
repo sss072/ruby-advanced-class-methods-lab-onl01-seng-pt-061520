a13f4a6462a14689d651e502068df10e63f02855
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-25 12:54:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,33 +3,36 @@ class Song
   @@all = []
 
   def self.all
+    @@all
+  end
 
   def save
     self.class.all << self
   end
+
   def self.create
-    song = self.new 
-    @@all << song
-    song 
-  end
-  def self.new_by_name(song_name)
-    song = self.new 
-    song.name = song_name
-    song 
-  end
-  def self.create_by_name(song_name)
-    song = self.new 
-    song.name = song_name 
-    @@all << song 
+    song = Song.new
+    song.save
     song
   end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
   def self.find_by_name(song_name)
-    @@all.find{|element| element.name == song_name}
-    @@all
+    self.all.detect{|s| s.name == song_name}
   end
-  end
+
   def self.find_or_create_by_name(song_name)
-    
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
-      
 end
