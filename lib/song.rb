4fdf2820013e8ebdb59dc8ab466c405842401705
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-25 12:47:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,5 +26,6 @@ class Song
     song
   end
   def self.find_by_name(song_name)
-    @@all.find{|element| element.name == song_name 
+    @@all.find{|element| element.name == song_name}
+  end
 end
